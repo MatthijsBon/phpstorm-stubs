7fc807d57ad21acf6c4c3c06d9d1659f55beba3b
Matthijs Bon <matthijs.bon@moxio.com>
2018-10-05 13:30:55 +0200

    Added stub class GEOSGeometry and some methods.

--- geosphp/geosphp.php
+++ geosphp/geosphp.php
@@ -1,3 +1,24 @@
 <?php
 
 function GEOSVersion() : string {};
+
+class GEOSGeometry {
+
+	/* General methods */
+	public function setSRID(int $int) : void {}
+
+	public function getSRID() : int {}
+
+	public function typeName() : string {}
+
+	/* Geometry operations */
+	public function length() : float {}
+
+	public function area() : float {}
+
+	public function distance() : float {}
+
+	/* Relational methods */
+	public function equalsExact(GEOSGeometry $other, float $epsilon) : bool {}
+
+}
